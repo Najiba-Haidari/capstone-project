c9d5c6bfc1f6ca1edfe08ea37eb5eb88788fe1d6
Najiba-Haidari <nhaidari089@gmail.com>
2024-06-03 09:35:14 -0600

    client added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {signupUser, loginUser} from '../controllers/userController.js'
 const router = express.Router();
 
-router.post("/", signupUser);
+router.post("/signup", signupUser);
 router.post("/login", loginUser)
 
 
