703b8240233fad46fceea8c8af12e9261f838165
Najiba-Haidari <nhaidari089@gmail.com>
2024-06-06 14:54:36 -0600

    prevent duplicating

--- client/src/ExerciseContext.jsx
+++ client/src/ExerciseContext.jsx
@@ -1,4 +1,4 @@
-import  { createContext, useState } from 'react';
+import { createContext, useState } from 'react';
 import axios from 'axios'
 
 // Create the context
@@ -11,29 +11,29 @@ export const ExerciseProvider = ({ children }) => {
 
   const saveExercise = async (exercise) => {
     const exerciseData = {
-        name: exercise.name,
-        gifUrl: exercise.gifUrl,
-        target: exercise.target,
-        equipment: exercise.equipment,
-        //   secondaryMuscles: exercise.secondaryMuscles,
+      name: exercise.name,
+      gifUrl: exercise.gifUrl,
+      target: exercise.target,
+      equipment: exercise.equipment,
+      //   secondaryMuscles: exercise.secondaryMuscles,
     };
     try {
-        if (!savedExercises.some(saved => saved.id === exercise.id)) {
-            await axios.post('http://localhost:3000/api/exercises', exerciseData);
-            setSavedExercises([...savedExercises, exercise]);
-            // setText(txt => ({...txt, [exercise.id]: "Saved"}))
-            alert('Exercise saved successfully!');
-        }
-        else {
-            alert('Exercise already saved!');
-        }
+      if (!savedExercises.find(saved => saved.name === exercise.name)) {
+        await axios.post('http://localhost:3000/api/exercises', exerciseData);
+        setSavedExercises([...savedExercises, exercise]);
+        // setText(txt => ({...txt, [exercise.id]: "Saved"}))
+        alert('Exercise saved successfully!');
+      }
+      else {
+        alert('Exercise already saved!');
+      }
     } catch (error) {
-        console.error('Error saving exercise', error);
+      console.error('Error saving exercise', error);
     }
-};
+  };
 
 
-const deleteExercise = async (exerciseId) => {
+  const deleteExercise = async (exerciseId) => {
     try {
       await axios.delete(`http://localhost:3000/api/exercises/${exerciseId}`);
       setSavedExercises(savedExercises.filter((exercise) => exercise._id !== exerciseId));
@@ -42,11 +42,11 @@ const deleteExercise = async (exerciseId) => {
       console.error('Error deleting exercise', error);
     }
   };
-  
+
 
 
   return (
-    <ExerciseContext.Provider value={{ savedExercises, setSavedExercises, saveExercise, deleteExercise}}>
+    <ExerciseContext.Provider value={{ savedExercises, setSavedExercises, saveExercise, deleteExercise }}>
       {children}
     </ExerciseContext.Provider>
   );
